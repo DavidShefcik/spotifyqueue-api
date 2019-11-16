0cf05eb79b62cfbffdea1f5ce74b4c6228c8c991
David Shefcik <davidshefcik@gmail.com>
2019-11-16 15:19:38 -0600

    Added .env and create basic server.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+/*
+* Author: David Shefcik
+* Created: 11/16/19
+* Project | File: Spotify Queue Authentication API | src/index.js
+* Purpose: Create express server, configure middleware for express server, and add routes to the express server.
+*/
+
 "use strict"
 
-console.log("Hello from index.js!");
+// Configure dotenv
+require("dotenv").config();
+
+// Module imports
+const express = require("express");
+
+// Create server
+const app = express();
+
+// Add/Configure middleware
+// TODO
+
+// Start server on port specified in .env file
+app.listen(process.env.PORT, () => {
+    console.log(`Authentication express server started on port ${process.env.PORT}!`);
+});
