6006aff91ca6c0ab2875be69426dbd625f3a2782
David Shefcik <davidshefcik@gmail.com>
2019-11-16 15:57:38 -0600

    Added user model and schema for mongoose.

---
+++ src/mongoose/models/userModel.js
@@ -0,0 +1,19 @@
+/*
+* Author: David Shefcik
+* Created: 11/16/19
+* Project | File: Spotify Queue Authentication API | src/mongoose/models/userModel.js
+* Purpose: Mongoose data model for the user to be inserted into the database.
+*/
+
+// Module imports
+const mongoose = require("mongoose");
+const path = require("path");
+
+// Schema import
+const userSchema = require(path.join(__dirname, "../schemas/userSchema"));
+
+// User model
+const userModel = mongoose.model("users", userSchema);
+
+// Export
+export default userModel;
---
+++ src/mongoose/schemas/userSchema.js
@@ -0,0 +1,18 @@
+/*
+* Author: David Shefcik
+* Created: 11/16/19
+* Project | File: Spotify Queue Authentication API | src/mongoose/schemas/userSchema.js
+* Purpose: Mongoose data schema for the user to be inserted into the database.
+*/
+
+// Module imports
+const mongoose = require("mongoose");
+
+// User schema
+const userSchema = new mongoose.Schema({
+    userid: "string",
+    token: "string",
+});
+
+// Export
+export default userSchema;
