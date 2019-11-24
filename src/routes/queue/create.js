52fdd871cd1514b03d110d3ae5cd420883891be6
David Shefcik <davidshefcik@gmail.com>
2019-11-23 22:09:27 -0600

    Create queue route.

--- src/routes/queue/create.js
+++ src/routes/queue/create.js
@@ -28,10 +28,73 @@ const queueModel = require(path.join(
 module.exports = app => {
   app.post('/queue/create', requireToken, (request, response) => {
     let token = request.headers.token
+    let id
+    let ownerid
+    let code
+    let codeOpen = false
+    let queue
     if (token === undefined) {
       return response.status(401).send({ error: 'unauthorized' })
     } else {
-      return response.status(200).send({ content: 'succ' })
+      userModel.findOne({ token: token }, (error, user) => {
+        if (error) {
+          if (process.env.PRODUCTION === 'false') {
+            console.log(error)
+          }
+          return response.status(400).send({ error: 'database' })
+        } else {
+          if (user) {
+            ownerid = user['userid']
+            id = require('crypto')
+              .randomBytes(64)
+              .toString('hex')
+
+            do {
+              code = Math.floor(Math.random() * 900000) + 100000
+              queueModel.find({ code: code }, (error, queues) => {
+                if (error) {
+                  if (process.env.PRODUCTION === 'false') {
+                    console.log(error)
+                  }
+                  codeOpen = true
+                  return response.status(400).send({ error: 'database' })
+                } else {
+                  if (!queues.length) {
+                    codeOpen = true
+                  }
+                }
+              })
+            } while (codeOpen)
+
+            queue = {
+              queueid: id,
+              ownerid: ownerid,
+              code: code,
+              memberids: [''],
+              songs: ['']
+            }
+
+            queueModel
+              .findOneAndUpdate({ ownerid: ownerid }, queue, {
+                new: true,
+                upsert: true,
+                useFindAndModify: false
+              })
+              .then(doc => {
+                console.log(doc)
+                return response.status(200).send({ queueid: queue['queueid'] })
+              })
+              .catch(error => {
+                if (process.env.PRODUCTION === 'false') {
+                  console.log(error)
+                }
+                return response.status(400).send({ error: 'database' })
+              })
+          } else {
+            return response.status(400).send({ error: 'nouser' })
+          }
+        }
+      })
     }
   })
 }
